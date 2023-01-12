063468a71deee2cadd8e91936e53386d65faf3d5
markuscoding <markangelomagnayon65@gmail.com>
2023-01-11 21:46:44 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home";
